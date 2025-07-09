9653623b46a95e43470f360bb8393f12967350b5
joaowonccepro <joao.woncce@prodigitalhr.com.br>
2025-07-09 09:25:04 -0300

    ilumeo-api: - cors config and example for usage

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import colaboradorRoute from '../routes/colaborador.route';
 import pontoRoute from '../routes/ponto.route';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}));
 
 // [joaovictor - 07/07/2025] Rotas principais
 app.use('/colaborador', colaboradorRoute);
